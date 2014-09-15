f138f7f433523174a0bace76e54b72132b9e0ea1
clinpsywoo <clinpsywoo@gmail.com>
2014-09-15 05:33:27 -0600

    add one functionality to wani_make_mask.m

--- matlab_functions/wani_make_mask.m
+++ matlab_functions/wani_make_mask.m
@@ -10,15 +10,29 @@ function wani_make_mask(fname, cl, varargin)
 %             fname = fullfile(outputdir, 'mask_DMPFC.img');
 % varargin options:
 %    'unique_mask_values': will save cl with different numbers
+%    'original_mask_values': will saven cl with original values
 %
 
 dounique = 0;
-if nargin > 2
-    dounique = 1;
+doorigi = 0;
+dobinary = 1;
+
+for i = 1:numel(varargin)
+    if ischar(varargin{i})
+        switch varargin{i}
+            case 'unique_mask_values'
+                dounique = 1;
+                dobinary = 0;
+            case 'original_mask_values'
+                doorigi = 1;
+                dobinary = 0;
+        end
+    end
 end
 
-if ~dounique
-    
+
+if dobinary
+
     % new method using region object
 %     try
 %         cldat = region2imagevec(cl);
@@ -50,14 +64,41 @@ elseif dounique
     cldat = replace_empty(cldat);
     dat = cldat;
     dat.dat = zeros(size(dat.dat));
-    
+
     for i = 1:numel(cl)
         cldat = region2imagevec(cl(i));
         cldat.dat(cldat.dat ~= 0) = 1;
         cldat = replace_empty(cldat);
         dat.dat(cldat.dat ~=0) = i;
     end
-    
+
+    dat.fullpath = fname;
+    write(dat);
+
+elseif doorigi
+    try
+        cldat = region2imagevec(cl(1));
+    catch
+        cl(1) = cluster2region(cl(1));
+        cldat = region2imagevec(cl(1));
+    end
+    cldat = replace_empty(cldat);
+    dat = cldat;
+    dat.dat = zeros(size(dat.dat));
+
+    for i = 1:numel(cl)
+        cldat = region2imagevec(cl(i));
+        cldat.dat(cldat.dat ~= 0) = 1;
+        cldat = replace_empty(cldat);
+        if ~isempty(cl(i).val)
+            dat.dat(cldat.dat ~=0) = cl(i).val;
+        elseif ~isempty(cl(i).Z)
+            dat.dat(cldat.dat ~=0) = cl(i).Z';
+        else
+            error('cl,val and cl.Z is empty');
+        end
+    end
+
     dat.fullpath = fname;
     write(dat);
 end
